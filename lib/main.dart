f690f782d25f1d06a8715f7692a50dd7eca4599a
eslamsalah5 <eslamsalah5346@gmail.com>
2023-08-05 01:08:53 +0300

    theme and me design done

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jodel_app/shared/components/widgets/bloc_observe.dart';
+import 'package:jodel_app/shared/network/local/cache_helper.dart';
+import 'package:jodel_app/shared/styles/mode/cubit.dart';
+import 'package:jodel_app/shared/styles/mode/state.dart';
+import 'package:jodel_app/shared/styles/themes.dart';
 import 'package:jodel_app/views/app_home_view/app_home_view.dart';
 import 'package:jodel_app/views/info_on_boarding_view/info_on_boarding.dart';
 import 'package:jodel_app/views/location_view/location_view.dart';
 import 'package:jodel_app/views/on_boarding/presentation/on_boarding_view.dart';
 import 'package:jodel_app/views/splash_view/splash_view.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  await CacheHelper.init();
+
+  bool? isDark = CacheHelper.getData(key: 'isDark');
+  runApp( MyApp(
+    isDark: isDark,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool? isDark;
+  const MyApp({super.key, this.isDark});
 
   // This widgets is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
-        routes: {
-          SplashView.pageID: (context) => const SplashView(),
-          OnBoardingView.pageID: (context) => const OnBoardingView(),
-          LocationView.pageID: (context) => const LocationView(),
-          InfoOnBoardingView.pageID: (context) => const InfoOnBoardingView(),
-          AppHomeView.pageID: (context) => const AppHomeView(),
-        },
-        initialRoute: SplashView.pageID,
-        debugShowCheckedModeBanner: false,
-        home: const SplashView(),
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => ModeCubit()
+              ..changeAppMode(
+                fromShared: isDark,
+              ),
+          ),
+        ],
+        child: BlocConsumer<ModeCubit, ModeStates>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              return ScreenUtilInit(
+                designSize: const Size(360, 690),
+                minTextAdapt: true,
+                splitScreenMode: true,
+                builder: (context, child) => MaterialApp(
+                  theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: ModeCubit.get(context).isDark
+              ? ThemeMode.dark 
+              : ThemeMode.light,
+                  routes: {
+                    SplashView.pageID: (context) => const SplashView(),
+                    OnBoardingView.pageID: (context) => const OnBoardingView(),
+                    LocationView.pageID: (context) => const LocationView(),
+                    InfoOnBoardingView.pageID: (context) =>
+                        const InfoOnBoardingView(),
+                    AppHomeView.pageID: (context) => const AppHomeView(),
+                  },
+                  initialRoute: SplashView.pageID,
+                  debugShowCheckedModeBanner: false,
+                  home: const SplashView(),
+                ),
+              );
+            }));
   }
 }
--- lib/views/app_home_view/app_home_view.dart
+++ lib/views/app_home_view/app_home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jodel_app/core/utils/constants.dart';
+import 'package:jodel_app/shared/styles/mode/cubit.dart';
+import 'package:jodel_app/shared/styles/mode/state.dart';
 import 'package:jodel_app/views/app_home_view/widgets/channels_view/channels_view.dart';
 import 'package:jodel_app/views/app_home_view/widgets/home_view/home_view.dart';
 import 'package:jodel_app/views/app_home_view/widgets/me_view/me_view.dart';
@@ -27,193 +30,206 @@ class _AppHomeViewState extends State<AppHomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Container(
-        height: 70,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    pageIndex = 0;
-                  });
-                },
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      pageIndex == 0
-                          ? Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 3),
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30)),
-                                color: kPrimaryColor,
-                              ),
-                              child: Image.asset(
-                                'assets/images/white_jodel_icon.png',
-                                width: 30,
-                              ),
-                            )
-                          : Image.asset(
-                              'assets/images/black_jodel_icon.png',
-                              width: 30,
+    return BlocConsumer<ModeCubit, ModeStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
+            bottomNavigationBar: Container(
+              height: 70,
+              decoration:  BoxDecoration(
+                color: ModeCubit.get(context).isDark
+                    ? const Color(0xff26252A)
+                    : Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          pageIndex = 0;
+                        });
+                      },
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            pageIndex == 0
+                                ? Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 15, vertical: 3),
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(30)),
+                                      color: kPrimaryColor,
+                                    ),
+                                    child: Image.asset(
+                                      'assets/images/white_jodel_icon.png',
+                                      width: 30,
+                                    ),
+                                  )
+                                : Image.asset(
+                                  ModeCubit.get(context).isDark ?
+                                    'assets/images/white_jodel_icon.png' : 'assets/images/black_jodel_icon.png' ,
+                                    width: 30,
+                                  ),
+                            const SizedBox(
+                              height: 3,
                             ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      const Text(
-                        'Home',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ]),
+                            const Text(
+                              'Home',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          pageIndex = 1;
+                        });
+                      },
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            pageIndex == 1
+                                ? Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 15, vertical: 3),
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(30)),
+                                      color: Colors.red,
+                                    ),
+                                    child: const Icon(
+                                      Icons.search_rounded,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  )
+                                :  Icon(
+                                    Icons.search_rounded,
+                               color: ModeCubit.get(context).isDark
+                                   ? const Color(0xffBFBEC3)
+                                   : const Color(0xff414045),
+                                    size: 30,
+                                  ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                            const Text(
+                              'Channels',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          pageIndex = 2;
+                        });
+                      },
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            pageIndex == 2
+                                ? Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 15, vertical: 3),
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(30)),
+                                      color: Colors.teal,
+                                    ),
+                                    child: const Icon(
+                                      Icons.notifications,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  )
+                                :  Icon(
+                                    Icons.notifications,
+                                  color: ModeCubit.get(context).isDark
+                                   ? const Color(0xffBFBEC3)
+                                   : const Color(0xff414045),
+                                    size: 30,
+                                  ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                            const Text(
+                              'Inbox',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          pageIndex = 3;
+                        });
+                      },
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            pageIndex == 3
+                                ? Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 15, vertical: 3),
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(30)),
+                                      color: Colors.blue[700],
+                                    ),
+                                    child: const Icon(
+                                      Icons.person,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  )
+                                :  Icon(
+                                    Icons.person,
+                                  color: ModeCubit.get(context).isDark
+                                   ? const Color(0xffBFBEC3)
+                                   : const Color(0xff414045),
+                                    size: 30,
+                                  ),
+                            const SizedBox(
+                              height: 3,
+                            ),
+                            const Text(
+                              'Me',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    pageIndex = 1;
-                  });
-                },
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      pageIndex == 1
-                          ? Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 3),
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30)),
-                                color: Colors.red,
-                              ),
-                              child: const Icon(
-                                Icons.search_rounded,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                            )
-                          : const Icon(
-                              Icons.search_rounded,
-                              color: Colors.black,
-                              size: 30,
-                            ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      const Text(
-                        'Channels',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ]),
-              ),
-            ),
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    pageIndex = 2;
-                  });
-                },
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      pageIndex == 2
-                          ? Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 3),
-                              decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30)),
-                                color: Colors.teal,
-                              ),
-                              child: const Icon(
-                                Icons.notifications,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                            )
-                          : const Icon(
-                              Icons.notifications,
-                              color: Colors.black,
-                              size: 30,
-                            ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      const Text(
-                        'Inbox',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ]),
-              ),
-            ),
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    pageIndex = 3;
-                  });
-                },
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      pageIndex == 3
-                          ? Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 15, vertical: 3),
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(30)),
-                                color: Colors.blue[700],
-                              ),
-                              child: const Icon(
-                                Icons.person,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                            )
-                          : const Icon(
-                              Icons.person,
-                              color: Colors.black,
-                              size: 30,
-                            ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      const Text(
-                        'Me',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ]),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: pages[pageIndex],
-    );
+            body: pages[pageIndex],
+          );
+        });
   }
 }
