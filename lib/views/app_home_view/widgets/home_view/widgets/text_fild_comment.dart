2c2417d8fb0d648f8d3f0ed061d228618c4e4114
eslamsalah5 <eslamsalah5346@gmail.com>
2023-08-19 20:19:09 +0300

    home themes and localizaion

--- lib/views/app_home_view/widgets/home_view/widgets/text_fild_comment.dart
+++ lib/views/app_home_view/widgets/home_view/widgets/text_fild_comment.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jodel_app/core/utils/constants.dart';
+import 'package:jodel_app/generated/l10n.dart';
+import 'package:jodel_app/shared/styles/mode/cubit.dart';
 
 class TextFieldComments extends StatefulWidget {
   const TextFieldComments({super.key});
@@ -14,20 +17,29 @@ class _TextFieldCommentsState extends State<TextFieldComments> {
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return TextFormField(
-      controller: user,
-      decoration: InputDecoration(
+    return Material(
+      elevation: 30,
+      shadowColor: Colors.green,
+      child: TextFormField(
+        controller: user,
+        decoration: InputDecoration(
           border: const OutlineInputBorder(),
-          hintText: '#!ردود ايجابيه فقط',
+          hintText: S.of(context).comment_text_form_hint,
           filled: true,
-          fillColor: Colors.white70,
+          fillColor:
+              ModeCubit.get(context).isDark ? Color(0xff26252A) : Colors.white,
           hintMaxLines: 2,
-          hintStyle: const TextStyle(
-            fontSize: 25,
+          hintStyle: TextStyle(
+            fontSize: 18,
+            color: ModeCubit.get(context).isDark ? Colors.grey : Colors.black,
           ),
-          suffixIcon:
-              IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt)),
-          suffixIconColor: Colors.black),
+          prefixIcon: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.camera_alt),
+            color: ModeCubit.get(context).isDark ? Colors.grey : Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
